e1d94c9189a070844ac05432ce3a97e5af90b907
woodghost <tingyue@vite.org>
2018-08-03 14:49:22 +0800

    i18n

--- i18n/en.js
+++ i18n/en.js
@@ -1,5 +1,18 @@
 module.exports = {
   lang: "English",
+  main :{
+    title: "Announcement of Preview Version",
+    ctx1: "Dear friends and community volunteers of Vite:",
+    ctx2: "The main network and testing network version 1.0 of Vite are under constructed right now, currently it is preview version.",
+    ctx3: "You can search snapshot blocks, transactions and account details through preview version of Vite blockchain explorer.",
+    ctx4: "Meanwhile, you can download the preview version of Vite wallet to generate individual account, apply testing tokens, make transactions and search transaction details via Vite blockchain explorer.",
+    ctx5: "Download preview version of Vite wallet",
+    ctx6: "Mac OS",
+    ctx7: "Windows",
+    ctx8: "View preview version of Vite blockchain explorer",
+    ctx9: "All of above system code have been open source on",
+    ctx10: "welcome to review and star us :)",
+  },
   footer: {
     official: "Official Website"
   }
