30020fc2681e274d63f1d60efa7c29047b13ac9d
woodghost <tingyue@vite.org>
2018-11-20 20:59:51 +0800

    nav & text fix

--- i18n/zh.js
+++ i18n/zh.js
@@ -4,7 +4,7 @@ module.exports = {
         title: "Vite-新一代高性能公链",
     },
     nav: {
-        explorer: '浏览器',
+        explorer: '区块浏览器',
         webWallet: 'Web钱包',
         desktopWallet: {
             name: '桌面钱包',
@@ -25,10 +25,10 @@ module.exports = {
     },
     main: {
         notice: {
-            title1: "Vite测试网络-Vite Testnet 1.0.0正式上线",
-            title2: "11月8日，Vite正式上线第一个测试网络版本Vite Testnet 1.0.0。同时，此前发布的区块浏览器，钱包，Vite Store等产品都已切换至Vite Testnet 1.0.0 网络并提供了部分新功能的入口。<br/>测试网络包括如下主要功能模块：",
+            title1: "Vite测试网络-Vite TestNet 1.0.0正式上线",
+            title2: "11月8日，Vite正式上线第一个测试网络版本Vite TestNet 1.0.0。同时，此前发布的区块浏览器，钱包，Vite Store等产品都已切换至Vite TestNet 1.0.0 网络并提供了部分新功能的入口。<br/>测试网络包括如下主要功能模块：",
             ctx:["DAG 账本结构","内置智能合约","HDPoS分层共识机制","异步通信 ","快照链", "铸币", "交易配额", "SBP注册和投票"],
-            summary: "目前，Vite系统共有三种代币：VITE，VCP（Vite Community Point），VTT（Vite Test Token）。<br/> Web钱包1.2.0及桌面版钱包1.2.0新增PoW、抵押获取配额及注册SBP和投票功能；iOS钱包新增PoW、抵押获取配额及投票功能。区块浏览器新增SBP列表页，支持查看投票情况。Vite Pay 接入Vite Testnet 1.0.0后支付速度达到秒级。Vite Store 新增数种虚拟商品。",
+            summary: "目前，Vite系统共有三种代币：VITE，VCP（Vite Community Point），VTT（Vite Test Token）。<br/> Web钱包1.1.0新增PoW、抵押获取配额及注册SBP和投票功能；iOS钱包新增PoW、抵押获取配额及投票功能。区块浏览器新增SBP列表页，支持查看投票情况。Vite Pay 接入Vite TestNet 1.0.0后支付速度达到秒级。Vite Store 新增数种虚拟商品。",
             annotation: '* SBP：快照块生产节点'
         },
         downloadWallet: {
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,8 @@
                 <div class="title">{{ $t('main.notice.title1') }}</div>
                 <div class="context">
                     <div class="preview" v-html="$t('main.notice.title2')"></div>
-                    <div class="inner" v-for="(i,j) in $t('main.notice.ctx')" :key="j"><span class="dot"></span> {{ i }}</div>
+                    <div class="inner" v-for="(i,j) in $t('main.notice.ctx')" :key="j"><span class="dot"></span> {{ i }}
+                    </div>
                     <div class="preview" v-html="$t('main.notice.summary')"></div>
                     <div class="preview annotation">{{ $t('main.notice.annotation') }}</div>
                 </div>
@@ -13,273 +14,285 @@
                     <div class="content-title">{{ $t('main.downloadWallet.title') }}</div>
                     <div class="version">
                         <el-popover
-                          placement="top"
-                          trigger="click">
-                          <div class="ios-container">
-                            <div v-for="(app, index) in apps" :key="index">
-                              <img :src="links.iosApp[app]"  class="ios-app" />
-                              <div>{{$t(`nav.iosApp.childs.${app}`)}}</div>
+                                placement="top"
+                                trigger="click">
+                            <div class="ios-container">
+                                <div v-for="(app, index) in apps" :key="index">
+                                    <img :src="links.iosApp[app]" class="ios-app"/>
+                                    <div>{{$t(`nav.iosApp.childs.${app}`)}}</div>
+                                </div>
                             </div>
-                          </div>
-                          <div class="version-btn btn-ios" slot="reference"><img src="~assets/images/applewhite.svg"/><span class="new-page">iOS</span></div>
+                            <div class="version-btn btn-ios" slot="reference"><img src="~assets/images/applewhite.svg"/><span
+                                    class="new-page">iOS</span></div>
                         </el-popover>
                         <a href="http://download.vite.net/Vite.Wallet-1.0.0.dmg">
-                            <div class="version-btn ml30"><img src="~assets/images/appleblue.svg"/><span class="new-page">Mac OS</span></div>
+                            <div class="version-btn ml30"><img src="~assets/images/appleblue.svg"/><span
+                                    class="new-page">Mac OS</span></div>
                         </a>
                         <a href="http://download.vite.net/Vite.Wallet-1.0.0.exe">
-                            <div class="version-btn ml30"><img src="~assets/images/windows.svg"/><span class="new-page">Windows</span></div>
+                            <div class="version-btn ml30"><img src="~assets/images/windows.svg"/><span class="new-page">Windows</span>
+                            </div>
                         </a>
                     </div>
                 </div>
                 <div class="explorer">
-                  <div class="content-title">{{ $t('main.links.title') }}</div>
+                    <div class="content-title">{{ $t('main.links.title') }}</div>
                     <a href="mailto:info@vite.org" target="_blank" class="extral">
-                      info@vite.org <img src="~assets/images/newpage.svg" class="new-page"/>
+                        info@vite.org <img src="~assets/images/newpage.svg" class="new-page"/>
                     </a>
                 </div>
             </div>
             <div class="right page-bottom">
                 <img src="~assets/images/computer.svg"/>
-      </div>
             </div>
-
         </div>
+
+    </div>
 </template>
 
 <script>
-import LangSelect from "~/components/LangSelect.vue";
-export default {
-  components: {
-    LangSelect
-  },
-  data() {
-    return {
-      apps: ['zh', 'en'],
-      links: {
-        iosApp: {
-          en: require('~/assets/images/waiapp.png'),
-          zh: require('~/assets/images/zhongapp.png')
+    import LangSelect from "~/components/LangSelect.vue";
+
+    export default {
+        components: {
+            LangSelect
+        },
+        data() {
+            return {
+                apps: ['zh', 'en'],
+                links: {
+                    iosApp: {
+                        en: require('~/assets/images/waiapp.png'),
+                        zh: require('~/assets/images/zhongapp.png')
+                    }
+                }
+            };
         }
-      }
     };
-  }
-};
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-@import "~assets/css/vars";
-a {
-  text-decoration: none;
-  cursor: pointer;
-}
-.dot {
-  display: inline-block;
-  width: 6px;
-  height: 6px;
-  border-radius: 6px;
-  background: #007AFF;
-  margin-right: 10px;
-}
-.content-title {
-  font-family: PingFangSC-Semibold;
-  font-size: 16px;
-  color: #272727;
-  letter-spacing: 0;
-  line-height: 18px;
-  margin-bottom: 20px;
-}
-.ios-container {
-    display: flex;
-    display: -webkit-flex;
-    padding: 20px 20px 20px 0;
-    font-size: 14px;
-    color: #5E6875;
-    letter-spacing: 0;
-    text-align: center;
-    .ios-app {
-      width: 100px;
-      height: 100px;
-      margin-left: 20px;
+    @import "~assets/css/vars";
+
+    a {
+        text-decoration: none;
+        cursor: pointer;
     }
-  }
-.ml30 {
-  margin-left: 30px;
-}
-.page-home {
-  font-family: Arial, PingFangSC-Semibold;
-  .lang-select {
-    float: right;
-    margin-right: 26px;
-    margin-top: 13px;
-    &:hover {
-      cursor: pointer;
+
+    .dot {
+        display: inline-block;
+        width: 6px;
+        height: 6px;
+        border-radius: 6px;
+        background: #007AFF;
+        margin-right: 10px;
     }
-  }
-  .page-content {
-    margin-top: 5%;
-    display: flex;
-    display: -webkit-flex;
-    color: #272727;
-    font-size: 18px;
-    letter-spacing: 0;
-    line-height: 18px;
-    .extral {
-      color: #053cce;
-    }
-    .page-bottom {
-      margin-bottom: 76px;
-    }
-    .left {
-      width: 50%;
-      .title {
-        font-size: 40px;
-        letter-spacing: 0.5px;
-        line-height: 46px;
-      }
-      .context {
-        margin-top: 33px;
-        line-height: 22px;
-        font-family: PingFangSC-Regular;
+
+    .content-title {
+        font-family: PingFangSC-Semibold;
+        font-size: 16px;
         color: #272727;
-        font-size: 12px;
-        .preview {
-          padding-bottom: 10px;
-        }
-        .annotation {
-          color: #5E6875;
-        }
-        .inner {
-          font-size: 12px;
-          text-indent: -10px;
-        }
-      }
-      .download {
-        margin-top: 30px;
-        margin-bottom: 30px;
-        &.app {
-          .qrcode {
-            margin-top:24px;
-            width: 110px;
-            height: 110px;
-            top: -120px;
-            background-image: url(~assets/images/code_app_ios.png);
-            background-size: 100px;
-            background-repeat: no-repeat;
-            background-position: center;
-            background-color: #fff;
-          }
-        }
-      }
-      .version {
+        letter-spacing: 0;
+        line-height: 18px;
+        margin-bottom: 20px;
+    }
+
+    .ios-container {
         display: flex;
         display: -webkit-flex;
-        .version-btn {
-          cursor: pointer;
-          width: 140px;
-          height: 46px;
-          text-align: center;
-          line-height: 46px;
-          background: #FFFFFF;
-          border: 2px solid #007AFF;
-          border-radius: 2px;
-          color: #007AFF;
-          .new-page {
-            margin-left: 10px;
-          }
-          
-          &.btn-ios {
-            background: #007AFF;
-            color: #ffffff;
-          }
-          
+        padding: 20px 20px 20px 0;
+        font-size: 14px;
+        color: #5E6875;
+        letter-spacing: 0;
+        text-align: center;
+        .ios-app {
+            width: 100px;
+            height: 100px;
+            margin-left: 20px;
         }
-      }
-      .explorer {
-        margin-bottom: 20px;
-        max-width: 400px;
-      }
     }
-    .right {
-      width: 50%;
-      & > img {
-        width: 100%;
-      }
-    }
-  }
-}
-@media (max-width: $MQNarrow) {
-}
 
-@media (max-width: $MQMobile) {
-  .content-wrapper {
-    padding: 0 1rem;
-  }
-  .page-home {
-    .logo {
-      width: 70px;
-      color: white;
-      margin-top: 10px;
+    .ml30 {
+        margin-left: 30px;
     }
-    .lang-select {
-      margin-right: 0;
-    }
-    .page-content {
-      display: flex;
-      flex-direction: column-reverse;
-      overflow: hidden;
-      margin: 2rem 0 0 0;
-      padding: 0;
-      width: 100%;
-      .right {
-        width: 100%;
-        float: left;
-        margin-bottom: 1rem;
-        text-align: center;
-        &.page-bottom {
-          margin-bottom: 0;
-        }
-        & > svg {
-          width: 300px;
-        }
-      }
-      .left {
-        width: 100%;
-        color: white;
-        text-align: center;
-        &.page-bottom {
-          margin-bottom: 0;
-        }
-        .title {
-          font-size: 1.2rem;
-        }
-        .context {
-          color: rgba(255, 255, 255, 0.9);
-          font-size: 0.9rem;
-          line-height: 1.5rem;
-        }
-        .download-wallet {
-          .version {
-            display: flex;
-            flex-direction: column;
-            align-content: center;
-            justify-content: center;
-            a {
-              text-align: center;
-              .wallet-version {
-                margin: 1rem 0 0 0;
-              }
+
+    .page-home {
+        font-family: Arial, PingFangSC-Semibold;
+        .lang-select {
+            float: right;
+            margin-right: 26px;
+            margin-top: 13px;
+            &:hover {
+                cursor: pointer;
             }
-          }
         }
-        .explorer {
-          text-align: center;
-          font-size: 0.95rem;
-          & > a {
-            color: #f7f8fb;
-          }
+        .page-content {
+            margin-top: 5%;
+            display: flex;
+            display: -webkit-flex;
+            color: #272727;
+            font-size: 18px;
+            letter-spacing: 0;
+            line-height: 18px;
+            .extral {
+                color: #053cce;
+            }
+            .page-bottom {
+                margin-bottom: 76px;
+            }
+            .left {
+                width: 50%;
+                .title {
+                    font-size: 40px;
+                    letter-spacing: 0.5px;
+                    line-height: 46px;
+                }
+                .context {
+                    margin-top: 33px;
+                    line-height: 22px;
+                    font-family: PingFangSC-Regular;
+                    color: #272727;
+                    font-size: 12px;
+                    .preview {
+                        padding: 10px 0;
+                    }
+                    .annotation {
+                        color: #5E6875;
+                    }
+                    .inner {
+                        margin-left: 10px;
+                        font-size: 12px;
+                        text-indent: -10px;
+                    }
+                }
+                .download {
+                    margin-top: 30px;
+                    margin-bottom: 30px;
+                    &.app {
+                        .qrcode {
+                            margin-top: 24px;
+                            width: 110px;
+                            height: 110px;
+                            top: -120px;
+                            background-image: url(~assets/images/code_app_ios.png);
+                            background-size: 100px;
+                            background-repeat: no-repeat;
+                            background-position: center;
+                            background-color: #fff;
+                        }
+                    }
+                }
+                .version {
+                    display: flex;
+                    display: -webkit-flex;
+                    .version-btn {
+                        cursor: pointer;
+                        width: 140px;
+                        height: 46px;
+                        text-align: center;
+                        line-height: 46px;
+                        background: #FFFFFF;
+                        border: 2px solid #007AFF;
+                        border-radius: 2px;
+                        color: #007AFF;
+                        .new-page {
+                            margin-left: 10px;
+                        }
+
+                        &.btn-ios {
+                            background: #007AFF;
+                            color: #ffffff;
+                        }
+
+                    }
+                }
+                .explorer {
+                    margin-bottom: 20px;
+                    max-width: 400px;
+                }
+            }
+            .right {
+                width: 50%;
+                & > img {
+                    width: 100%;
+                }
+            }
         }
-      }
     }
-  }
-}
+
+    @media (max-width: $MQNarrow) {
+    }
+
+    @media (max-width: $MQMobile) {
+        .content-wrapper {
+            padding: 0 1rem;
+        }
+        .page-home {
+            .logo {
+                width: 70px;
+                color: white;
+                margin-top: 10px;
+            }
+            .lang-select {
+                margin-right: 0;
+            }
+            .page-content {
+                display: flex;
+                flex-direction: column-reverse;
+                overflow: hidden;
+                margin: 2rem 0 0 0;
+                padding: 0;
+                width: 100%;
+                .right {
+                    width: 100%;
+                    float: left;
+                    margin-bottom: 1rem;
+                    text-align: center;
+                    &.page-bottom {
+                        margin-bottom: 0;
+                    }
+                    & > svg {
+                        width: 300px;
+                    }
+                }
+                .left {
+                    width: 100%;
+                    color: white;
+                    text-align: center;
+                    &.page-bottom {
+                        margin-bottom: 0;
+                    }
+                    .title {
+                        font-size: 1.2rem;
+                    }
+                    .context {
+                        color: rgba(255, 255, 255, 0.9);
+                        font-size: 0.9rem;
+                        line-height: 1.5rem;
+                    }
+                    .download-wallet {
+                        .version {
+                            display: flex;
+                            flex-direction: column;
+                            align-content: center;
+                            justify-content: center;
+                            a {
+                                text-align: center;
+                                .wallet-version {
+                                    margin: 1rem 0 0 0;
+                                }
+                            }
+                        }
+                    }
+                    .explorer {
+                        text-align: center;
+                        font-size: 0.95rem;
+                        & > a {
+                            color: #f7f8fb;
+                        }
+                    }
+                }
+            }
+        }
+    }
 </style>
