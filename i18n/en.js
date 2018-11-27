49bd3a4b2ad8cbd3c7b82c0d96f92a4a1be4f0f9
woodghost <tingyue@vite.org>
2018-11-27 18:22:05 +0800

    minor text fix

--- i18n/en.js
+++ i18n/en.js
@@ -33,7 +33,7 @@ module.exports = {
             ctx: ["Vite TestNet incentive plan started at 13:30 (UTC+8:00) Nov 23, 2018",
                 "In TestNet, Vite Fund is responsible for at least 5 SBPs to ensure the stability of the system. All of the rewards garnered by official SBPs will be given back to our community members",
                 "Official SBPs of Vite: <span style='color:#007AFF;font-family: PingFangSC-Semibold;'>Vite_SBP01、Vite_SBP02、Vite_SBP03、Vite_SBP04、Vite_SBP05</span>",
-                "The rewards obtained by these 5 SBPs will be as a whole, 70% of which will be distributed to users who vote for us. Users voted for any of these 5 SBPs can obtain rewards, the left 30% will be issued to full nodes.",
+                "The rewards obtained by these 5 SBPs will be as a whole, 70% of which will be distributed to users who vote for us. Users voted for any of these 5 SBPs can obtain rewards, the left 30% will be rewarded to full nodes.",
                 "These 5 SBPs are expected to generate <span style='color:#007AFF;font-family: PingFangSC-Semibold;'>8000+ </span>VITE as voting rewards, the earlier you vote, the more rewards you garner!"],
         },
         downloadWallet: {
