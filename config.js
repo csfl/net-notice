35fd698aac005eec0c438486f7a89c597ba84af3
lovelycs <419579547@qq.com>
2018-12-03 15:22:17 +0800

    update desktopWallet download url

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ export default {
     viteStore: 'https://vite.store',
     website: 'https://vite.org',
     desktopWallet: {
-      mac: 'http://download.vite.net/Vite.Wallet-1.0.0.dmg',
-      win: 'http://download.vite.net/Vite.Wallet-1.0.0.exe'
+      mac: 'http://download.vite.net/ViteWallet-1.2.0.dmg',
+      win: 'http://download.vite.net/ViteWallet-1.2.0.exe'
     },
     iosApp: {
       en: require('~/assets/images/waiapp.png'),
